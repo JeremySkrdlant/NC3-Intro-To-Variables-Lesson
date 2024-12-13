09f977cbea9cda7039ebf8745007c3b6e9bd2c30
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2024-12-13 14:35:44 -0700

    Initial Commit

---
+++ VariablesDemo/VariablesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariablesDemoApp.swift
+//  VariablesDemo
+//
+//  Created by Jeremy Skrdlant on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct VariablesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VariablesDemoTests/VariablesDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  VariablesDemoTests.swift
+//  VariablesDemoTests
+//
+//  Created by Jeremy Skrdlant on 12/13/24.
+//
+
+import Testing
+@testable import VariablesDemo
+
+struct VariablesDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
